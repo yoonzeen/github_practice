df4abf69f48e506fd0aaa28599cc59c09fcf07a4
zinny <zinny@yanadoocorp.com>
2022-12-28 11:53:11 +0900

    [ADD] git 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='content'>
+        안녕하세요. <br/>
+        깃헙 연동을 해보겠습니다.
+      </div>
     </div>
   );
 }
